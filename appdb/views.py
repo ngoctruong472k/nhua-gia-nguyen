9129e1bb0d09b6ff5c9382802d70a59483de6f5e
Nghia Nguyen <nghianguyen.js1060@gmail.com>
2022-06-15 14:57:47 +0700

    add two page contact and introduce

--- appdb/views.py
+++ appdb/views.py
@@ -9,10 +9,18 @@ from flask import render_template
 @app.route("/")
 def home():
 	
-    return render_template('home/layout/layout.html')
+    return render_template('index.html')
 
 
 @app.route('/product')
 def product():
 
-    return render_template('products/nain.html')
+    return render_template('products/product.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('home/layout/contact.html')
+
+@app.route('/introduce')
+def introduce():
+    return render_template('home/layout/introduce.html')
